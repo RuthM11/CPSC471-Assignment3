e8c2dee0b073d9461a1a968a55e37fc669c0f441
nathansmith339 <smithnathan339@gmail.com>
2017-05-02 23:22:13 -0700

    created connection information file

--- client.py
+++ client.py
@@ -6,26 +6,78 @@ from connectioninfo import *
 # connectioninfo.py contains the information to easily edit it in the future
 serverName = SERVER_NAME
 serverPort = PORT_NUMBER
-
-# create a socket, connect
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-
-# string to send
-data2 = ""
+inputData = ""
+PARTITION_SIZE = 40
 
 
-while str(data2).lower() != "exit":
+# connects to a socket using server an port as arguments
+def client_connect_tcp(server, port):
     try:
-        data2 = input("send a string (type exit to exit): ")
-        if str(data2).lower() == "exit":
-            break
-        elif str(data2).isprintable():
-            clientSocket.send(bytes(data2, 'utf-8'))
-    except ConnectionAbortedError:
-        print("Connection aborted, closing.\n")
+        connSocket = socket(AF_INET, SOCK_STREAM)
+        connSocket.connect((server, port))
+
+    except ConnectionRefusedError as e:
+        # happens when client cannot connect to server
+        print("Client can't connect. Ensure server is running.\nMessage: %s" % e)
+
     finally:
-        print("Message sent.\n")
-# close the socket
-clientSocket.close()
-print("Connection closed.\n")
+        return connSocket
+
+def send_data(data, dataSock):
+    bytesSent = 0
+    sectionNumber = 1
+
+
+    while bytesSent <= len(data):
+        try:
+            sectionBytes = 0
+            # input("say something: ")
+
+            # send data part by part
+            sectionBytes += dataSock.send(bytes(data[bytesSent:(PARTITION_SIZE * sectionNumber)], 'utf-8'))
+            sectionNumber += 1
+
+            # fill in section if it's too small because the server
+            # is waiting for all the data (can also do a timeout)
+            while sectionBytes < PARTITION_SIZE:
+                sectionBytes += dataSock.send(bytes(str('\0'), 'utf-8'))
+
+            # keep track of total bytes sent
+            bytesSent += sectionBytes
+
+            # progress report
+            print("Total bytes sent so far: %d" % bytesSent)
+
+        except ConnectionRefusedError as e:
+            # happens when server is down and client tries to send
+            print("Error with connection: %s" % e)
+            break
+
+        except ConnectionAbortedError as e:
+            # happens when server cuts connection and client tries to send
+            print("Error with connection: %s" % e)
+            break
+
+        finally:
+            # break if we're done
+            if bytesSent >= len(data):
+                break
+
+
+while str(inputData).lower() != "exit":
+
+    inputData = input("Send a string (type exit to exit): ")
+    if str(inputData).lower() == "exit":
+        break
+
+    # connect and send
+    dataSocket = client_connect_tcp(serverName, serverPort)
+    send_data(inputData, dataSocket)
+    print("Message sent")
+
+    # close the socket when the message is done being sent
+    # print("Closing connection")
+    dataSocket.close()
+    # print("Connection closed.\n")
+
+
--- server.py
+++ server.py
@@ -16,16 +16,29 @@ print("Server ready to listen.\n")
 
 #data buffer
 data = ""
+tmpBuff = bytes()
+BYTES_TO_ACCEPT = 40
 
 while 1:
     # Accept a connection request: get the client's socket
     connectionSocket, addr = serverSocket.accept()
+    print("%s says: " % str(addr[0]), end="")
+    #connectionSocket.settimeout(3)
 
-    # Receive whatever the newly conneted client has sent
-    data = connectionSocket.recv(40)
+    try:
+        # Receive whatever the newly conneted client has sent
+        while len(bytes(data, 'utf-8')) != BYTES_TO_ACCEPT:
+            tmpBuff = connectionSocket.recv(BYTES_TO_ACCEPT)
+            if not tmpBuff:
+                print("Socket closed unexpectedly.")
+                break
+            data += tmpBuff.decode('utf-8')
+    except timeout:
+        pass
 
-    print(data.decode('utf-8'))
+    finally:
+        print(data)
+        data = ""
+        #tmpBuff = bytes("", 'utf-8')
 
-    # Close the socket after receipt
     connectionSocket.close()
-
